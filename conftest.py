e56d6a1f0d49993fe976efe94a29cb0905bcf291
TkachevAlex <itbr.taxes@gmail.com>
2023-04-03 23:12:44 +0300

    Step 4

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language')
+    parser.addoption('--language', action='store', default='en')
 
 
 @pytest.fixture(scope="function")
