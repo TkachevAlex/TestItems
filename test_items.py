54813797f8583225be443f92a9184c93b341d8e9
TkachevAlex <itbr.taxes@gmail.com>
2023-04-03 22:22:37 +0300

    My test_items file_edit

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_add_button(browser):
     browser.get(link)
     time.sleep(30)
     add_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
