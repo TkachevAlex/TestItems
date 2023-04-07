48ad2c039d59e58213efd5639af8817b9e404e6f
TkachevAlex <itbr.taxes@gmail.com>
2023-04-08 01:36:53 +0300

    Finish

--- PgObject_course/pages/base_page.py
+++ PgObject_course/pages/base_page.py
@@ -1,7 +1,10 @@
 import math
-
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+
+from pages.locators import BasePageLocators
 
 
 class BasePage():
@@ -20,6 +23,22 @@ class BasePage():
             return False
         return True
 
+    def is_not_element_present(self, how, what, timeout=4):
+        # упадет, как только увидит искомый элемент. Не появился: успех, тест зеленый.
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        # будет ждать до тех пор, пока элемент не исчезнет.
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -33,3 +52,7 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
